a2761dfd86121605b4f1d0b130b264cc06da9a09
Sebastian Locksenius <slocksenius@gmail.com>
2023-11-13 15:08:42 +0100

    added styling to Home, added another nav for desktop, added category specific styling on listings

---
+++ frontend/src/components/DesktopNav.tsx
@@ -0,0 +1,20 @@
+import { BiCar } from "react-icons/bi";
+import { RiMotorbikeLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+
+const DesktopNav = () => {
+  return (
+    <nav className='desktop-navlinks'>
+        <Link to='/car' className='desktop-navlinks-car'>
+          <BiCar classname='desktop-pic-car' size={28} />
+          <span>Bil</span>
+        </Link>
+        <Link to='/motorcycle' className='desktop-navlinks-mc'>
+          <span>MC</span>
+          <RiMotorbikeLine classname='desktop-pic-mc' size={28} />
+        </Link>
+      </nav>
+  )
+}
+
+export default DesktopNav
--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FaLocationPin } from "react-icons/fa6";
+import DesktopNav from "../components/DesktopNav"
 
 const Home = () => {
   const [listings, setListings] = useState<Listing[]>([]); //using useState hook to create listing state variable as an array
@@ -17,24 +19,31 @@ const Home = () => {
   }, []);
 
   return (
-    <ul className="home-container">
-      {listings.map(listing => (
-        <li className="home-listing" key={listing._id}>
+    <div className="outer-container">
+      < DesktopNav/>
+      <ul className="home-container">
+        {listings.map(listing => (
+          <li className="home-listing" key={listing._id}>
           <div className="listing-left">
             <img src={listing.imageURL}></img>
-            <p>{listing.city}</p>
-            <p>{listing.price}kr /dygn</p>
+            <div className="listing-left-bottomleft">
+              <p> <span className={listing.category === "MC" ? "mc-color" : "car-color"}>< FaLocationPin /></span> {listing.city}</p>
+            </div>
+            <div className="listing-left-bottomright">
+              <p className={listing.category === "MC" ? "mc-bcolor-tran" : "car-bcolor-tran"}>{listing.price}kr /dygn</p>
+            </div>
           </div>
           <div className="listing-right">
             <p>Kategori: {listing.category}</p>
-            <p>{listing.description}</p>
+            <p>{listing.description.length > 45 ? listing.description.substring(0, 45) + "..." : listing.description}</p> {/* limits displayed lenght */}
             <p className="listing-btn-container">
-                <Link to={`/${listing._id}`}>RESERVERA</Link>
+              <Link className={listing.category === "MC" ? "mc-bcolor" : "car-bcolor"} to={`/${listing._id}`}>RESERVERA</Link>
             </p>
           </div>
         </li>
-      ))}
-    </ul>
+        ))}
+      </ul>
+    </div>
   )
 }
 export default Home
