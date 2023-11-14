b8f73605e8017278574ab8446687b8fa1ce36434
Sebastian Locksenius <slocksenius@gmail.com>
2023-11-14 23:47:42 +0100

    Added checkout get and checkout post booking functionality

--- frontend/src/views/Checkout.tsx
+++ frontend/src/views/Checkout.tsx
@@ -1,7 +1,96 @@
+import { useEffect, useState, useContext } from 'react';
+import { FaLocationPin } from "react-icons/fa6";
+import { AuthContext } from "../context/AuthContext";
+
 
 const Checkout = () => {
+  const [listing, setListing] = useState<Listing>(); //using useState hook to create listing state variable as an array
+  const [error, setError] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const listingData = localStorage.getItem('listingData');
+  const parsedData = listingData !== null ? JSON.parse(listingData) : null;
+  const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    if (parsedData !== null) {  //if parsedData isnt null
+      const listingId = parsedData.listingId;
+
+      fetch(`http://localhost:7777/api/listings/${listingId}`)
+        .then(res => {
+          if (!res.ok) {
+            throw new Error('Listing not found');
+          }
+          return res.json();
+        })
+        .then(data => {
+          setListing(data);
+          setTotalPrice(data.price * parsedData.dates.length); //calculate total price
+        })
+        .catch(error => {
+          console.error(error);
+          setError(true);
+        });
+
+    } else {  //if parsedData is null
+      setError(true);
+    }
+
+  }, [parsedData]);
+  if (error) {  //if there is an error
+    return <div className='checkout-error'><h1>Ingen bokning (0)</h1></div>
+  }
+  if (!listing) {
+    return <div className='checkout-error'><h1>Laddar...</h1></div>
+  }
+
+  const handleBooking = () => {
+  if (!authContext) {
+    return
+  }
+  const token = authContext.token;
+    fetch('http://localhost:7777/api/bookings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: listingData,
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  };
+
   return (
-    <div>Checkout</div>
+    <div className='checkout-container'>
+      <h1>Checkout (1)</h1>
+      <div className='checkout-card'>
+        <div className="checkout-card-upper">
+          <img src={listing.imageURL}></img>
+          <div className="checkout-card-upper-bottomleft">
+            <p> <span className={listing.category === "MC" ? "mc-color" : "car-color"}>< FaLocationPin /></span> {listing.city}</p>
+          </div>
+          <div className="checkout-card-upper-bottomright">
+            <p className={listing.category === "MC" ? "mc-bcolor-tran" : "car-bcolor-tran"}>{listing.price}kr /dygn</p>
+          </div>
+          <div className="checkout-card-upper-upperright">
+
+          </div>
+        </div>
+        <div className="checkout-card-lower">
+          <p>Datum: {parsedData && parsedData.dates.join(', ')}</p>
+          <p>Pris: {totalPrice}kr</p>
+        </div>
+      </div>
+      <div className="checkout-btn-container">
+      <button onClick={handleBooking}>Betala Nu (Totalt: {totalPrice}kr)</button>
+      </div>
+    </div>
+
   )
 }
 
