8f833f1f83b7004c5090728efa3f5d24128504e0
Sebastian Locksenius <slocksenius@gmail.com>
2023-11-15 20:32:03 +0100

    finished payment page

--- frontend/src/views/Payment.tsx
+++ frontend/src/views/Payment.tsx
@@ -1,7 +1,21 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from 'react';
+import { AuthContext } from "../context/AuthContext";
 
 const Payment = () => {
+  const authContext = useContext(AuthContext);
+  const navigate = useNavigate();
+  if (!authContext?.token) {
+    navigate("/login")
+  }
   return (
-    <div><h1>Betalning sker... Vänligen vänta.</h1></div>
+    <div className="payment-container">
+      <div className="payment-topcover"></div>
+      <div className="payment-container2">
+        <h2>Betalning sker... Vänligen vänta.</h2>
+        <Link to="/bookings">Gå vidare (Mina Parkeringar)</Link>
+      </div>
+    </div>
   )
 }
 
