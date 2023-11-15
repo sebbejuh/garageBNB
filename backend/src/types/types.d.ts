ee3b5c229fa238fbc226669eeafc47b71a8f4e86
Sebastian Locksenius <slocksenius@gmail.com>
2023-11-15 22:28:59 +0100

    type edits

--- backend/src/types/types.d.ts
+++ backend/src/types/types.d.ts
@@ -13,15 +13,3 @@ interface RequestWithUserData extends Request {
     email: string;
   };
 }
-
-interface Listing {
-  address: string;
-  category: string;
-  city: string;
-  description: string;
-  host: string;
-  hostImgURL: string;
-  imageURL: string;
-  price: number;
-  _id: string;
-}
