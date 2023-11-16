e383b69f2886cfc2044d1f1ba585222c4ad4afea
Sebastian Locksenius <slocksenius@gmail.com>
2023-11-16 21:20:27 +0100

    SearchBar component now fully functional

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,11 +1,13 @@
 import { AiOutlineSearch } from "react-icons/ai";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 const SearchBar = () => {
-  const [input, setInput] = useState("");
-  const [results, setResults] = useState<Listing[]>([]);
+  const [input, setInput] = useState(''); //input state for input field
+  const [results, setResults] = useState<Listing[]>([]);  //state for results from fetch
+  const ref = useRef<HTMLDivElement>(null); //reference to my div, used in useEffect hook
 
+  //Function that gets all listings from db and filters them by city depending on input
   const fetchData = (value: string) => {
     fetch("http://localhost:7777/api/listings/")
       .then((res) => res.json())
@@ -21,34 +23,55 @@ const SearchBar = () => {
       });
   };
 
+  //useEffect that keeps track of where you click, if it's outside of ref it empties the states
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setInput('');
+        setResults([]);
+      }
+    }
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   const handleChange = (value: string) => {
     setInput(value);  //updates state
     fetchData(value); //runs function with value
   };
 
   return (
-    
-      <div className='navbar-search'>
-        <div className="navbar-search-static">
-          <AiOutlineSearch size={28} />
-          <input
-            placeholder='Stockholm...'
-            type="text"
-            onChange={(e) => handleChange(e.target.value)}
-          />
-        </div>
-      {
-        results.map((result, index) => (
-          <div className="navbar-search-results" key={index}>
-            <div className="test">
-
-            <Link className="search-results-link" to={`/${result._id}`}>{result.city}</Link>
-            </div>
-          </div>
-        ))
-      }
+    <div className='navbar-search'>
+      <div className="navbar-search-static">
+        <AiOutlineSearch size={28} />
+        <input
+          placeholder='Stockholm...'
+          type="text"
+          value={input}
+          onChange={(e) => handleChange(e.target.value)}
+        />
       </div>
-    
+      <div className="navbar-search-results-wrapper" ref={ref}>
+        {
+          results.map((result, index) => (
+            <div className="navbar-search-results" key={index}>
+              <Link
+                className="search-results-link"
+                to={`/${result._id}`}
+                onClick={() => {
+                  setInput('');
+                  setResults([]);
+                }}
+              >
+                {result.city} Pris: {result.price}kr
+              </Link>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
