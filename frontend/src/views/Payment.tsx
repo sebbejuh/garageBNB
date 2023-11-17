64ef2d9e0c2d9c05bf984076ed48be28380dd90a
Sebastian Locksenius <slocksenius@gmail.com>
2023-11-17 22:08:07 +0100

    finished payment styling and styled gobackbtn some more

--- frontend/src/views/Payment.tsx
+++ frontend/src/views/Payment.tsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useContext } from 'react';
 import { AuthContext } from "../context/AuthContext";
+import GoBackBtn from "../components/GoBackBtn"
 
 const Payment = () => {
   const authContext = useContext(AuthContext);
@@ -9,13 +10,16 @@ const Payment = () => {
     navigate("/login")
   }
   return (
-    <div className="payment-container">
-      <div className="payment-topcover"></div>
-      <div className="payment-container2">
-        <h2>Betalning sker... Vänligen vänta.</h2>
-        <Link to="/bookings">Gå vidare (Mina Parkeringar)</Link>
+    <>
+      <GoBackBtn />
+      <div className="payment-container">
+        {/* <div className="payment-topcover"></div> */}
+        <div className="payment-container2">
+          <h2>Betalning sker... Vänligen vänta.</h2>
+          <Link to="/bookings">Gå vidare (Mina Parkeringar)</Link>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
